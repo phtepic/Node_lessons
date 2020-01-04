59a3c2ddc7f3fddef7308dd0631a88e1f2310b02
phtepic <58693558+phtepic@users.noreply.github.com>
2020-01-04 13:38:21 +0100

    socketio simple chat added

---
+++ 06-socketio_simple_chat/client/index.js
@@ -0,0 +1,22 @@
+const socket = require("socket.io-client")("http://localhost:3000");
+const repl = require("repl");
+const chalk = require("chalk");
+
+socket.on("disconnect", function() {
+  socket.emit("disconnect");
+});
+socket.on("connect", () => {
+  console.log(chalk.red("- start chatting -"));
+  username = process.argv[2];
+});
+socket.on("message", (data) => {
+  const { cmd, username } = data;
+  console.log(chalk.green(username + ": " + cmd.split("\n")[0]));
+});
+
+repl.start({
+  promt: "",
+  eval: (cmd) => {
+    socket.send({ cmd, username });
+  }
+});
