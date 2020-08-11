728b17102ec83610b992cf145001d6978a5c8181
Dominik Rumian <drumian@seznam.cz>
2020-08-11 07:59:07 +0200

    comments to python files added

--- Python_soketIO/client.py
+++ Python_soketIO/client.py
@@ -1,8 +1,15 @@
+# It's neccessay to install package 'python-socketio' to be able to use this
+# import (pip3 install python-socketio)
 import socketio
+# Needs to be imported to make work with JSONs possible
 import json
 
+# Creates client instance
 sio = socketio.Client()
 
+# Following functions are event handlers: they are called whenever a particular
+# event happens. For complete set of events see documentation.
+# https://python-socketio.readthedocs.io/
 @sio.event
 def connect():
     print('connection established')
@@ -15,13 +22,23 @@ def connect():
 
 @sio.event
 def message(data):
-    # msg = json.loads(data)
-    # print('Recieved message: ', data['msg'])
-    print(data)
+    # Incoming JSON has format {"type": "msg" "msg": "SOME MESSAGE"}
+    # It needs to be parsed using json.loads() method - it converts JSON like
+    # string to Python dictionary
+    m = json.loads(data)
+    print('Recieved message: ', m['msg'])
 
 @sio.event
 def disconnect():
     print('disconnected from server')
 
+# Connects client to specified host
 sio.connect('http://localhost:11000')
+# Makes the client wait until the server takes an action
 sio.wait()
+
+"""
+If we want to do something in particular, we omit the 'sio.wait()' statement and
+we write some code instead - this code will be executed (e.g. endless loop which
+is sending data somewhere)
+"""
--- Python_soketIO/server.py
+++ Python_soketIO/server.py
@@ -1,15 +1,33 @@
+# For server it's neccessay to install package 'eventlet' which manages 'listening'
+# of the server to the chosen port (pip3 install -U eventlet)
 import eventlet
+# It's neccessay to install package 'python-socketio' to be able to use this
+# import (pip3 install python-socketio)
 import socketio
+# Needs to be imported to make work with JSONs possible
 import json
 
+# First we need to create server instance
 sio = socketio.Server()
+# This makes the server send forward any traffic that is not intended for it.
+# It makes easier to integrate the server with other apps.
+# Note: static_files parameter configures the server, for the complete reference
+# see the documentation (https://python-socketio.readthedocs.io/)
 app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
 
+# Following functions are event handlers: they are called whenever a particular
+# event happens. For complete set of events see documentation.
+# https://python-socketio.readthedocs.io/
 @sio.event
 def connect(sid, environ):
-    sio.emit('message', 'Python server test message')
+    msg = {"type" : "msg", "msg": "Python server test message"}
+    out = json.dumps(msg)
+    # The first argument of this method is event name. Events can be of the
+    # following types: 'connect', 'message', 'disconnect'. According to
+    # specified type, the server will react to the action
+    sio.emit('message', out)
     print('Client connected: ', sid)
 
 @sio.event
@@ -20,11 +38,14 @@ def disconnect(sid):
 # event occures
 @sio.event
 def message(sed, data):
-    # Incoming JSON has format 'type: "msg" msg: "JS client test msg"'
-    # It behaves like a dictionary by default -> so it's possible to call
-    # dict['key'] and it returns asociated value
-    print(data['msg'])
+    # Incoming JSON has format {"type": "msg" "msg": "SOME MESSAGE"}
+    # It needs to be parsed using json.loads() method - it converts JSON like
+    # string to Python
+
+    message = json.loads(data)
+    print(message['msg'])
 
 
 if __name__ == '__main__':
+    # This starts the server event loop
     eventlet.wsgi.server(eventlet.listen(('', 11000)), app)
